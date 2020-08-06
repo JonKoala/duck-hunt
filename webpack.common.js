67dc0274a626ebd9c5e5c7c6a4a749199132a9c3
JonKoala <victor.santana.lemos@gmail.com>
2020-08-06 12:18:32 -0300

    make the creation of html files exclusive to the dev build

--- webpack.common.js
+++ webpack.common.js
@@ -1,10 +1,3 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-
 module.exports = {
-  entry: './src/app.js',
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: 'duck-hunt'
-    })
-  ]
+  entry: './src/app.js'
 }
